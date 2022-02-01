d7208c126a47175bbdc7990e1c80da8dff3f952a
muhal24 <76661007+muhal24@users.noreply.github.com>
2022-02-01 22:39:42 +0300

    Initial Commit

---
+++ ExchangeApp/ExchangeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeAppApp.swift
+//  ExchangeApp
+//
+//  Created by Muhammed Hanifi Alma on 1.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
